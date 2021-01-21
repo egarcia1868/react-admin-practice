1dab9555be5ad9d8b29679c3ca2558c8769d6a3d
Eric Garcia <egarcia1868@gmail.com>
2021-01-20 21:58:34 -0600

    added a dummy login page that you have to enter fake credentials before entering

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import UserList from "./components/UserList";
 import { PostList, PostEdit, PostCreate } from "./components/PostList";
 import UserIcon from "@material-ui/icons/Group";
 import PostIcon from "@material-ui/icons/Book";
+import authProvider from "./components/AuthProvider";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin authProvider={authProvider} dashboard={Dashboard} dataProvider={dataProvider}>
     <Resource name="users" icon={UserIcon} list={UserList} />
     <Resource name="posts" icon={PostIcon} list={PostList} edit={PostEdit} create={PostCreate} />
   </Admin>
