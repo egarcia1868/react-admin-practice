5a7403666cae617b1fbc38264805c768677be8c9
Eric Garcia <egarcia1868@gmail.com>
2021-01-20 21:09:07 -0600

    created some search filters

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { 
+import {
+  Filter, 
   List,
   Datagrid,
   TextField,
@@ -13,8 +14,17 @@ import {
   TextInput,
 } from 'react-admin';
 
+const PostFilter = props => (
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn />
+    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+      <SelectInput optionText="name" />
+    </ReferenceInput>  
+ </Filter>
+)
+
 const PostList = props => (
-    <List {...props}>
+    <List filters={<PostFilter />} {...props}>
         <Datagrid>
           <TextField source="id" />
           <ReferenceField source="userId" reference="users">
@@ -27,7 +37,7 @@ const PostList = props => (
 );
 
 const PostEdit = props => (
-  <Edit {...props}>
+  <Edit title={<PostTitle />}{...props}>
     <SimpleForm>
       <TextInput disabled source="id" />
       <ReferenceInput source="userId" reference="users">
@@ -38,6 +48,10 @@ const PostEdit = props => (
     </SimpleForm>
   </Edit>
 );
+
+const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ""}</span>;
+}
 
 const PostCreate = props => (
   <Create {...props}>
